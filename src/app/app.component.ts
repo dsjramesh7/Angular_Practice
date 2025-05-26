7cb98379949209cbb01d2aedc2f75ef2f18070a6
dsjramesh7 <rameshdussa07@gmail.com>
2025-05-26 23:35:58 +0530

    rpactice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, HeaderComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Angula Practice ';
+  message = 'just chilll just chill';
+  imageUrl =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUPIfiGgUML8G3ZqsNLHfaCnZK3I5g4tJabQ&s';
+  handleClick() {
+    console.log('you clicked me !!!!!!!!!!!!');
+  }
   x = 12;
   y = 13;
+  isVisible = false;
+  username = '';
 }
